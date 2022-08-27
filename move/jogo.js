9bc036eb75e58f47f9ab91571800244bb6504a0b
TiagoHucs <tiagohucs@gmail.com>
2022-08-27 20:33:58 -0300

    ajustes

--- move/jogo.js
+++ move/jogo.js
@@ -1,7 +1,7 @@
 console.log('MOVES');
 
 const sprites = new Image();
-sprites.src = './sprites/MainGuySpriteSheet.png';
+sprites.src = './sprites/player1.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
@@ -9,15 +9,15 @@ const contexto = canvas.getContext('2d');
 const hero = {
     spriteX: 0,
     spriteY: 0,
-    largura: 50,
-    altura: 50,
+    largura: 28,
+    altura: 32,
     x: 50,
     y: 50,
     movimentos: [
         {spriteX: 0,spriteY: 0},
-        {spriteX: 50,spriteY: 0},
-        {spriteX: 100,spriteY: 0},
-        {spriteX: 50,spriteY: 0},
+        {spriteX: 28,spriteY: 0},
+        {spriteX: 56,spriteY: 0},
+        {spriteX: 84,spriteY: 0},
     ],
     incremento: 0,
     spriteAtual: 0,
@@ -25,7 +25,7 @@ const hero = {
        if(this.incremento >= 10){
         this.spriteAtual = this.spriteAtual >= 3 ? 0 : this.spriteAtual+1;
         this.incremento = 0;
-        this.y = this.y+4; //TODO: remover
+        this.y = this.y+1; //TODO: remover
        } else {
         this.incremento++;
        }
